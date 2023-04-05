c6c5d7459a761e28a24b2b7da3afae89f586bb15
tonisco <tonyumeozulu@gmail.com>
2023-04-05 11:02:50 +0100

    switched to pnpm

--- backend/db/config.ts
+++ backend/db/config.ts
@@ -5,19 +5,21 @@ let imageBucket: GridFSBucket
 let cvBucket: GridFSBucket
 
 const db = async () => {
-	try {
-		const connect = await mongoose.connect(process.env.MONGO_URI!)
-		console.log(`db connected on ${connect.connection.host}`)
-	} catch (error) {
-		console.log(error)
-		process.exit()
-	}
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI!)
+        console.log(`db connected on ${connect.connection.host}`)
+    } catch (error) {
+        console.log(error)
+        process.exit()
+    }
 }
 
 mongoose.connection.on("connected", () => {
-	const db = mongoose.connections[0].db
-	imageBucket = new mongoose.mongo.GridFSBucket(db, { bucketName: "images" })
-	cvBucket = new mongoose.mongo.GridFSBucket(db, { bucketName: "cvs" })
+    const db = mongoose.connections[0].db
+    // @ts-ignore
+    imageBucket = new mongoose.mongo.GridFSBucket(db, { bucketName: "images" })
+    // @ts-ignore
+    cvBucket = new mongoose.mongo.GridFSBucket(db, { bucketName: "cvs" })
 })
 
 export { imageBucket, cvBucket }
